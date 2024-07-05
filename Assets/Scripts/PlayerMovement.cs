7754b9be5b860cec83fb10482c329aa22aab9a0b
Rudra Pratap Chauhan <rcpython2@gmail.com>
2024-07-05 17:03:32 -0400

    Added the mouse look
    (beta 1.0.1)

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x;
+        float y;
+    }
+}
