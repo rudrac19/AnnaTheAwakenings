6148575876d0ee9cc44dea6104db0735a1efee61
Rudra Pratap Chauhan <rcpython2@gmail.com>
2024-07-05 22:21:40 -0400

    added movement
    (beta 1.0.2)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -8,10 +9,13 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed = 12f;
 
-    // Update is called once per frame
     void Update()
     {
-        float x;
-        float y;
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
     }
 }
