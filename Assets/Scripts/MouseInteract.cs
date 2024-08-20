19a27f4507ddb1013cf808b586b4cd0367b574dd
Rudra Pratap Chauhan <rcpython2@gmail.com>
2024-08-20 19:53:23 -0400

    Added more keys
    (beta 1.0.6)

--- Assets/Scripts/MouseInteract.cs
+++ Assets/Scripts/MouseInteract.cs
@@ -45,11 +45,11 @@ public class MouseInteract : MonoBehaviour
 
                 if (Input.GetButtonDown("Intereact"))
                 {
-                    GameObject key = hit.collider.gameObject;
-                    Destroy(key);
+                    GameObject gameObject = hit.collider.gameObject;
+                    Destroy(gameObject); //subject to change
                 }
             }
-            else if (hit.collider.gameObject.name == "Chest Key")
+            else if (hit.collider.gameObject.name == "Floor Key")
             {
                 foreach (GameObject uI in uIS)
                 {
@@ -59,8 +59,162 @@ public class MouseInteract : MonoBehaviour
 
                 if (Input.GetButtonDown("Intereact"))
                 {
-                    GameObject key = hit.collider.gameObject;
-                    Destroy(key);
+                    GameObject gameObject = hit.collider.gameObject;
+                    Destroy(gameObject);//subject to change
+                }
+            }
+            else if (hit.collider.gameObject.name == "Barrel Key")
+            {
+                foreach (GameObject uI in uIS)
+                {
+                    uI.SetActive(false);
+                }
+                uIS[3].SetActive(true);
+
+                if (Input.GetButtonDown("Intereact"))
+                {
+                    GameObject gameObject = hit.collider.gameObject;
+                    Destroy(gameObject);//subject to change
+                }
+            }
+            else if (hit.collider.gameObject.name == "Safe Key")
+            {
+                foreach (GameObject uI in uIS)
+                {
+                    uI.SetActive(false);
+                }
+                uIS[4].SetActive(true);
+
+                if (Input.GetButtonDown("Intereact"))
+                {
+                    GameObject gameObject = hit.collider.gameObject;
+                    Destroy(gameObject);//subject to change
+                }
+            }
+            else if (hit.collider.gameObject.name == "Padlock Key")
+            {
+                foreach (GameObject uI in uIS)
+                {
+                    uI.SetActive(false);
+                }
+                uIS[5].SetActive(true);
+
+                if (Input.GetButtonDown("Intereact"))
+                {
+                    GameObject gameObject = hit.collider.gameObject;
+                    Destroy(gameObject);//subject to change
+                }
+            }
+            else if (hit.collider.gameObject.name == "Crowbar")
+            {
+                foreach (GameObject uI in uIS)
+                {
+                    uI.SetActive(false);
+                }
+                uIS[6].SetActive(true);
+
+                if (Input.GetButtonDown("Intereact"))
+                {
+                    GameObject gameObject = hit.collider.gameObject;
+                    Destroy(gameObject);//subject to change
+                }
+            }
+            else if (hit.collider.gameObject.name == "Chest Key")
+            {
+                foreach (GameObject uI in uIS)
+                {
+                    uI.SetActive(false);
+                }
+                uIS[7].SetActive(true);
+
+                if (Input.GetButtonDown("Intereact"))
+                {
+                    GameObject gameObject = hit.collider.gameObject;
+                    Destroy(gameObject);//subject to change
+                }
+            }
+            else if (hit.collider.gameObject.name == "Red Key")
+            {
+                foreach (GameObject uI in uIS)
+                {
+                    uI.SetActive(false);
+                }
+                uIS[8].SetActive(true);
+
+                if (Input.GetButtonDown("Intereact"))
+                {
+                    GameObject gameObject = hit.collider.gameObject;
+                    Destroy(gameObject);//subject to change
+                }
+            }
+            else if (hit.collider.gameObject.name == "Blue Key")
+            {
+                foreach (GameObject uI in uIS)
+                {
+                    uI.SetActive(false);
+                }
+                uIS[9].SetActive(true);
+
+                if (Input.GetButtonDown("Intereact"))
+                {
+                    GameObject gameObject = hit.collider.gameObject;
+                    Destroy(gameObject);//subject to change
+                }
+            }
+            else if (hit.collider.gameObject.name == "Black Key")
+            {
+                foreach (GameObject uI in uIS)
+                {
+                    uI.SetActive(false);
+                }
+                uIS[10].SetActive(true);
+
+                if (Input.GetButtonDown("Intereact"))
+                {
+                    GameObject gameObject = hit.collider.gameObject;
+                    Destroy(gameObject);//subject to change
+                }
+            }
+            else if (hit.collider.gameObject.name == "Floor 2 Door")
+            {
+                foreach (GameObject uI in uIS)
+                {
+                    uI.SetActive(false);
+                }
+                uIS[0].SetActive(true);
+
+                if (Input.GetButtonDown("Intereact"))
+                {
+                    GameObject gameObject = hit.collider.gameObject;
+                    uIS[0].SetActive(true);
+                }
+            }
+            else if (hit.collider.gameObject.name == "Chest")
+            {
+                foreach (GameObject uI in uIS)
+                {
+                    uI.SetActive(false);
+                }
+                uIS[0].SetActive(true);
+
+                if (Input.GetButtonDown("Intereact"))
+                {
+                    GameObject gameObject = hit.collider.gameObject;
+                    uIS[0].SetActive(true);
+                }
+            }
+            else if (hit.collider.gameObject.name == "Barrels")
+            {
+                foreach (GameObject uI in uIS)
+                {
+                    uI.SetActive(false);
+                }
+                uIS[0].SetActive(true);
+
+                if (Input.GetButtonDown("Intereact"))
+                {
+                    GameObject gameObject = hit.collider.gameObject;
+                    uIS[0].SetActive(true);
                 }
             }
             else
@@ -73,7 +227,4 @@ public class MouseInteract : MonoBehaviour
             }
         }
     }
-
-    
-
 }
